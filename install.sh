fe0ab3f63c73c1d847c3d1436b9d02f61e7aff58
JankyGaming <JamesDonaldChilds@gmail.com>
2022-06-25 12:07:27 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,41 @@
+#TODO: Use getopts to make this a little more flexable
+
+user=$1
+if [ -z $user ] 
+then
+        printf "Please pass a username as only argument\n"
+        exit 1
+fi
+
+mkdir -p /backups/timemachine
+addgroup timemachine
+chown root:timemachine /backups/timemachine
+chmod 0770 /backups/timemachine
+
+#TODO: Check if user already exists
+
+adduser $user -G timemachine
+
+apk update
+
+# TODO: Check these services are already installed
+
+apk add dbus
+apk add avahi
+apk add netatalk
+rc-update add dbus
+rc-update add avahi-daemon
+rc-update add netatalk
+
+cp /etc/afp.conf /etc/afp.conf.bak
+echo "[Global]
+
+[TimeMachine]
+path = /timemachine
+time machine = yes
+valid users = username @${user}
+" > /etc/afp.conf
+
+/etc/init.d/dbus start
+/etc/init.d/avahi-daemon start
+/etc/init.d/netatalk start
